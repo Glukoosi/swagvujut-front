8e0cb37009d822dea2f84a81ca86dc55c4d92179
Aleksi Seppänen <aleksi@seppanen.dev>
2022-08-25 16:54:26 +0300

    Add history option

--- js/index.js
+++ js/index.js
@@ -2,27 +2,33 @@ import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 import { createApp, reactive } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js"
 import axios from 'https://cdn.skypack.dev/axios';
 
+const capacity = 5;
+const slug = "xwagtest";
+
+const openTimestamp = new Date("2021-09-01T10:37:00.000Z");
+const closeTimestamp = new Date("2022-09-30T20:59:00.000Z");
+
 const registered = reactive({
-    capacity: 64,
-    names: [],
+    capacity: capacity,
+    entries: null,
     getRegistered() {
-        axios.get("https://api.ilmo.io/api/registrations/names/xwagtest")
+        axios.get(`https://api.ilmo.io/api/registrations/names/${slug}`)
             .then(data => {
-                this.names = data.data.reverse();
+                this.entries = data.data.reverse();
 
             })
 
         const socket = io("https://api.ilmo.io");
 
-        socket.on("xwagtest", (msg) => {
-            this.names.push(msg)
+        socket.on(slug, (msg) => {
+            this.entries.push(msg)
         });
     }
 })
 
 const formTimer = reactive({
-    openTimestamp: new Date("2021-09-01T10:37:00.000Z"),
-    closeTimestamp: new Date("2022-09-30T20:59:00.000Z"),
+    openTimestamp: openTimestamp,
+    closeTimestamp: closeTimestamp,
     timestamp: new Date(),
     timer() {
         setInterval(() => {
@@ -43,12 +49,13 @@ createApp({
         food: "",
         alcohol: "",
         bottle: "",
+        history: "",
         tablewish: "",
         free: "",
         checkbox: "",
     },
     send() {
-        axios.post("https://api.ilmo.io/api/registration/xwagtest", this.form)
+        axios.post(`https://api.ilmo.io/api/registration/${slug}`, this.form)
         .then(() => {
             for (const item in this.form) {
                 this.form[item] = '';
